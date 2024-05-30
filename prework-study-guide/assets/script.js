5ce1b6cbec6d84584e85854a529c630110ed8e39
nramos9071 <nramos9071@gmail.com>
2024-05-30 11:08:33 -0500

    <added java script>

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,19 @@
+var topic = ['HTML', 'CCS', 'Git', 'JavaScript'];
+for (var x = 0; x < topic.length; x++) {
+    console.log(topic[x]);
+}
+
+/*
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topic === 'CCS') {
+    console.log("Let's study CCS!");
+} else if (topic === 'Git') {
+    console.log("Let's Study Git!");
+} else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+} else {
+    console.log('Please Try Again!');
+}
+*/
+
